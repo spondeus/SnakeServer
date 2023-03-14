149347c70ef07d18e1a971f7e62759019d38a80c
Tom Ofella <spondeus@gmail.com>
2023-03-14 12:07:22 +0100

    Initial commit

---
+++ src/main/java/pentasnake/server/snakeserver/SnakeServerApplication.java
@@ -0,0 +1,13 @@
+package pentasnake.server.snakeserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SnakeServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SnakeServerApplication.class, args);
+    }
+
+}
---
+++ src/test/java/pentasnake/server/snakeserver/SnakeServerApplicationTests.java
@@ -0,0 +1,13 @@
+package pentasnake.server.snakeserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SnakeServerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
