1c48dfee07b59197cfdacf587d78bf28a5d075ed
Virga Balint <sidosi70@gmail.com>
2023-03-19 15:17:36 +0100

    added Kryonet, basic server-side functionality

--- src/main/java/snakeserver/dir/RunServer.java
+++ src/main/java/snakeserver/dir/RunServer.java
@@ -1,14 +1,34 @@
 package snakeserver.dir;
 
+import com.esotericsoftware.kryonet.Server;
+import jakarta.annotation.PreDestroy;
+import lombok.AllArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.ApplicationArguments;
+import org.springframework.boot.ApplicationRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import snakeserver.dir.server.ServerController;
+
+import java.security.SecureRandom;
 
 @SpringBootApplication
-public class RunServer
-{
+public class RunServer implements ApplicationRunner {
+
+    @Autowired
+    private Server kryonetServer;
 
     public static void main(String[] args) {
         SpringApplication.run(RunServer.class, args);
     }
 
+    @Override
+    public void run(ApplicationArguments args) throws Exception {
+
+    }
+
+    @PreDestroy
+    public void stop() {
+        kryonetServer.stop();
+    }
 }
