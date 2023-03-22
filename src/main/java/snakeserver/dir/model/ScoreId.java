5cf5eb527df48dd8ac462f3f25d937dc5f6695fe
OzsvathHunor <45239731+OzsvathHunor@users.noreply.github.com>
2023-03-22 15:05:47 +0100

    Scoreboard remove

--- src/main/java/snakeserver/dir/model/ScoreId.java
+++ src/main/java/snakeserver/dir/model/ScoreId.java
@@ -1,10 +1,12 @@
 package snakeserver.dir.model;
 
 import jakarta.persistence.Embeddable;
+import lombok.EqualsAndHashCode;
 
 import java.io.Serializable;
 
 @Embeddable
+@EqualsAndHashCode
 public class ScoreId implements Serializable {
 
     private Long snake_player_id;
