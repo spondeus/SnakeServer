5f49893de7bbf8239056438acfd3669070ed8399
Virga Balint <sidosi70@gmail.com>
2023-03-23 12:07:38 +0100

    server, dependencies,

--- src/main/java/snakeserver/dir/RunServer.java
+++ src/main/java/snakeserver/dir/RunServer.java
@@ -1,15 +1,45 @@
 package snakeserver.dir;
 
+import lombok.val;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.event.EventListener;
 import snakeserver.dir.server.ServerSocket;
+
+import java.io.IOException;
+import java.util.Scanner;
 
 @SpringBootApplication
 public class RunServer {
 
+
+    @Autowired
+    private ServerSocket socket;
+    public static boolean restart;
+
     public static void main(String[] args) {
         SpringApplication.run(RunServer.class, args);
 
+    }
+
+    private void restartServer(){
+        val scn = new Scanner(System.in);
+        while(true) {
+            String command = scn.nextLine();
+
+            switch (command) {
+                case "restart_server"->{
+                    try{
+                        socket.stop();
+                        socket.start();
+                        System.out.println("[Console]: Server restarted");
+                    } catch (IOException | InterruptedException e){
+                        throw new RuntimeException(e);
+                    }
+                }
+            }
+        }
     }
 }
 
--- src/main/java/snakeserver/dir/server/ServerSocket.java
+++ src/main/java/snakeserver/dir/server/ServerSocket.java
@@ -6,8 +6,8 @@ import org.java_websocket.handshake.ClientHandshake;
 import org.java_websocket.server.WebSocketServer;
 import org.springframework.stereotype.Component;
 
-import java.util.ArrayList;
-import java.util.List;
+import javax.print.DocFlavor;
+import java.util.*;
 
 @Component
 public class ServerSocket extends WebSocketServer{
@@ -15,6 +15,8 @@ public class ServerSocket extends WebSocketServer{
     public static ServerSocket socket;
 
     private final List<Client> clients = new ArrayList<>();
+
+    private Map<WebSocket,String> snakeConstructs = new HashMap<>();
 
     public ServerSocket(InetSocket address){
         super(address);
@@ -42,7 +44,6 @@ public class ServerSocket extends WebSocketServer{
 
         webSocket.send("you're the: "+clients.size()+". player in the lobby");
 
-
     }
 
     @Override
@@ -51,15 +52,48 @@ public class ServerSocket extends WebSocketServer{
         clients.removeIf(x -> x.getRemoteAddress() == clientAddress);
 
         System.out.println("Client disconnected: "+ clientAddress);
+
+        for(var x: snakeConstructs.keySet()){
+            if(x == webSocket)
+                snakeConstructs.remove(x);
+        }
     }
 
     @Override
     public void onMessage(WebSocket webSocket, String s){
         System.out.println(webSocket.getRemoteSocketAddress() + ": " + s);
-        webSocket.send("ANYADAT LBGDX");
 
-        for(var x: clients){
-            x.getWebSocket().send("cons,"+s);
+
+        if(s.startsWith("cons")){
+            val xCord = new Random().nextInt(100,500);
+            val yCord = new Random().nextInt(100,500);
+
+            val msg = s.substring(4);
+            val builder = new StringBuilder();
+            val split = msg.split(",");
+            for(var x: split){
+                if(x.equals("?1")){
+                    builder.append(xCord).append(",");
+                } else if (x.equals("?2")){
+                    builder.append(yCord).append(",");
+                } else{
+                    builder.append(x).append(",");
+                }
+            }
+            snakeConstructs.put(webSocket, builder.substring(0, builder.length()-1));
+            System.out.println(builder.substring(0, builder.length()));
+        }
+
+        if(clients.size() == 1){
+            val string = new StringBuilder();
+            string.append("cons#");
+
+            for(var x: snakeConstructs.values()){
+                string.append(x).append("#");
+            }
+            for(var x: clients){
+                x.getWebSocket().send(string.toString());
+            }
         }
 
     }
@@ -72,10 +106,10 @@ public class ServerSocket extends WebSocketServer{
     @Override
     public void onStart(){
         System.out.println(
-                        """
-                            ////////////////////////////////
-                                    Server Started
-                            ////////////////////////////////
-                            """);
+                """
+                    ////////////////////////////////
+                            Server Started
+                    ////////////////////////////////
+                    """);
     }
 }
