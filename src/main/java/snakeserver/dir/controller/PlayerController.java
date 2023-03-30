b31324ffe682bc31cdeac5795b81bdb834ccff82
Tom Ofella <spondeus@gmail.com>
2023-03-30 21:32:04 +0200

    login-reg merged, not working

--- src/main/java/snakeserver/dir/controller/PlayerController.java
+++ src/main/java/snakeserver/dir/controller/PlayerController.java
@@ -6,7 +6,6 @@ import org.springframework.security.crypto.bcrypt.BCrypt;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
-import org.springframework.validation.ObjectError;
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.*;
 import snakeserver.dir.authetication.PlayerService;
