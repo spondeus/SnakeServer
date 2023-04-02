3be8e05ea0260cdd0f34807022fd248545213d88
Hunor <ozshunor@gmail.com>
2023-04-02 15:08:02 +0200

    bug fixed in saveServices

--- src/main/java/snakeserver/dir/model/GameRepository.java
+++ src/main/java/snakeserver/dir/model/GameRepository.java
@@ -1,5 +1,6 @@
 package snakeserver.dir.model;
 
+import org.jetbrains.annotations.NotNull;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 
@@ -8,6 +9,7 @@ public interface GameRepository extends JpaRepository<Game, Long> {
     @Query("select max(id) from Game")
     Long findMaxId();
 
+    @NotNull
     @Override
     Game getReferenceById(Long aLong);
 }
