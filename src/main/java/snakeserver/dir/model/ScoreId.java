4d4502fe0d7e715938075dcc9a270036765427ee
Hunor <ozshunor@gmail.com>
2023-04-02 16:11:14 +0200

    constructors added to entity classes

--- src/main/java/snakeserver/dir/model/ScoreId.java
+++ src/main/java/snakeserver/dir/model/ScoreId.java
@@ -11,4 +11,13 @@ public class ScoreId implements Serializable {
 
     private Long snake_player_id;
     private Long snake_game_id;
+
+    public ScoreId(Long id, Long id1) {
+        this.snake_player_id = id;
+        this.snake_game_id = id1;
+    }
+
+    public ScoreId() {
+
+    }
 }
