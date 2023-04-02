a442ca2473c76a5640eaade89989184647a4e7a8
Hunor <ozshunor@gmail.com>
2023-04-02 16:06:24 +0200

    remove Getter, setter annotations from ScoreId

--- src/main/java/snakeserver/dir/model/ScoreId.java
+++ src/main/java/snakeserver/dir/model/ScoreId.java
@@ -2,15 +2,11 @@ package snakeserver.dir.model;
 
 import jakarta.persistence.Embeddable;
 import lombok.EqualsAndHashCode;
-import lombok.Getter;
-import lombok.Setter;
 
 import java.io.Serializable;
 
 @Embeddable
 @EqualsAndHashCode
-@Setter
-@Getter
 public class ScoreId implements Serializable {
 
     private Long snake_player_id;
